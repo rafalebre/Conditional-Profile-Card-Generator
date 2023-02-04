8faa5f34a21b749792d3355e3418abc5be4f3427
rafalebre <rafaellebre100@gmail.com>
2023-02-04 12:30:09 +0000

    Just the social media button to be done

--- src/js/app.js
+++ src/js/app.js
@@ -44,10 +44,10 @@ function render(variables = {}) {
     variables.country
   )}</h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/rslebre"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/rafalebre"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/rafael-s-lebre/"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://www.instagram.com/rafalebre/"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
